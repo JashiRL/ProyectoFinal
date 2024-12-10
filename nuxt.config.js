b05d269da21b2b4f96e7fa4272080f251b621a77
Jashi RL <110833770+JashiRL@users.noreply.github.com>
2024-12-09 23:26:34 -0600

    Try to fix deployment run and build problems

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,10 +23,10 @@ export default {
   },
   target: 'static',
 
-  server: {
-    port: process.env.PORT, // default 3000
-    host: '0.0.0.0' // default: localhost
-  },
+  // server: {
+  //   port: process.env.PORT, // default 3000
+  //   host: '0.0.0.0' // default: localhost
+  // },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
